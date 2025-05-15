3eca4e1721ec17db6003f9440f42c71721af707f
flbusom <latorre.ferran@gmail.com>
2025-05-15 14:48:15 +0200

    update

--- PIIS-FlexiCoche_front-develop/js/vendors/bootstrap-loader.js
+++
@@ -1,8 +0,0 @@
-const linkBootstrap = document.createElement('link');
-linkBootstrap.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css';
-linkBootstrap.rel = 'stylesheet';
-document.head.appendChild(linkBootstrap);
-
-const scriptBootstrap = document.createElement('script');
-scriptBootstrap.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js';
-document.body.appendChild(scriptBootstrap);
--- PIIS-FlexiCoche_front-develop/js/vendors/fontawesome-loader.js
+++
@@ -1,6 +0,0 @@
-const faCSS = document.createElement('link');
-faCSS.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
-faCSS.rel = 'stylesheet';
-faCSS.integrity = 'sha512-...';
-faCSS.crossOrigin = 'anonymous';
-document.head.insertBefore(faCSS, document.head.firstChild);
