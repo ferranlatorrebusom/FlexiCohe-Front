446801e6ba2f6c99b4e2a1dbd9b370492fc17124
Ferran Josep Latorre Busom <fjlatorre@alu.ucam.edu>
2025-05-16 14:00:31 +0200

    Update car-create.page.js

--- js/modules/pages/car-create.page.js
+++ js/modules/pages/car-create.page.js
@@ -161,7 +161,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
             const token = localStorage.getItem('jwtToken');
 
-            const response = await fetch('/vehiculos/add', {
+            const response = await fetch(`${API_BASE}/vehiculos/add`, {
                 method: 'POST',
                 body: formData,
                 headers: {
